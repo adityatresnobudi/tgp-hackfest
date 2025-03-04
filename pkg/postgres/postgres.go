dd623fa6147d227c3956c3928dd341b27c68dd9a
aditbuddy <adityatresnobudi@gmail.com>
2025-03-04 09:48:30 +0700

    fix : tax from percentage to actual calculation

--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -99,28 +99,28 @@ func InitializeTable(db *sql.DB) error {
 	);`
 
 	// q7 := `
-	// 	INSERT INTO users (id, name, phone_number, has_octopay) VALUES 
+	// 	INSERT INTO users (id, name, phone_number, has_octopay) VALUES
 	// 	('f5063dca-556c-4723-931b-cbade7ca139a', 'Irfan', '081111111111', true),
 	// 	('3d91b359-b840-470b-a069-a4627900ddcf', 'Adit', '081290882428', false);
 	// `
 
 	// q8 := `
-	// 	INSERT INTO receipts (id, name, sum, discount, total, category, creator_id) VALUES 
+	// 	INSERT INTO receipts (id, name, sum, discount, total, category, creator_id) VALUES
 	// 	('ad9197e5-cb9a-419c-b055-596962d5501e', 'Test Toko', 38850, 0, 38850, 'Makanan', 'f5063dca-556c-4723-931b-cbade7ca139a');
 	// `
 
 	// q9 := `
-	// 	INSERT INTO payments (payment_method, payment_value, receipt_id, user_id) VALUES 
+	// 	INSERT INTO payments (payment_method, payment_value, receipt_id, user_id) VALUES
 	// 	('octopay', '081111111111', 'ad9197e5-cb9a-419c-b055-596962d5501e', 'f5063dca-556c-4723-931b-cbade7ca139a'),
 	// 	('qris', '00020101021126680016ID.CO.TELKOM.WWW011893600898026724611002150001952559966270303UMI51440014ID.CO.QRIS.WWW02150000002672461100303UMI5204573253033605502015802ID5916InterActive Corp6015Permata Safira 610560136630403A2', 'ad9197e5-cb9a-419c-b055-596962d5501e', '3d91b359-b840-470b-a069-a4627900ddcf');
 	// `
 
 	// q10 := `
-	// 	INSERT INTO items (product, price, quantity, discount, tax, service, total, receipt_id, user_id) VALUES 
-	// 	('Nasi Goreng', '20000', '1', '0', '11', '0', '22200', 'ad9197e5-cb9a-419c-b055-596962d5501e', 'f5063dca-556c-4723-931b-cbade7ca139a'),
-	// 	('Bakso', '15000', '1', '0', '11', '0', '16650', 'ad9197e5-cb9a-419c-b055-596962d5501e', '3d91b359-b840-470b-a069-a4627900ddcf');
+	// 	INSERT INTO items (product, price, quantity, discount, tax, service, total, receipt_id, user_id) VALUES
+	// 	('Nasi Goreng', '20000', '1', '0', '2200', '0', '22200', 'ad9197e5-cb9a-419c-b055-596962d5501e', 'f5063dca-556c-4723-931b-cbade7ca139a'),
+	// 	('Bakso', '15000', '1', '0', '1650', '0', '16650', 'ad9197e5-cb9a-419c-b055-596962d5501e', '3d91b359-b840-470b-a069-a4627900ddcf');
 	// `
-	
+
 	if _, err := db.Exec(q1); err != nil {
 		log.Printf("initialize table users_octo: %s\n", err.Error())
 		return err
